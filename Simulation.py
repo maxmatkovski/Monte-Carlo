99df3e240e901f557f2e4374513b19fc2986cea8
Max <74901101+datadrivenmax@users.noreply.github.com>
2023-05-12 22:03:43 -0700

    first commit

---
+++ Simulation.py
@@ -0,0 +1,26 @@
+'''
+Implementation of Monte Carlo method to simulate stock portfolio
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+from pandas_datareader import data as pdr
+
+# import data
+
+def get_data(stocks, start, end):
+    stockData = pdr.get_data_yahoo(stocks, start, end)
+    stockData = stockData ['Data']
+    returns = stockData.pct_change()
+    meanReturns = returns.mean()
+    covMatrix = returns.cov()
+    return meanReturns, covMatrix
+
+stockList = ['CBA','BHP','TLS','NAB','WBC','STO']
+stocks = [stock + '.AX' for stock in stockList]
+
+# parameter very important for covariance matrix which will affect monte carlo simulation. 
+endDate = dt.datetime.now()
+startDate = endDate - dt.timedelta(days=300)
